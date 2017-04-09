8f7b7077c6592fcaf4102e6c337f5521ee64f56c
suebtas <suebtas@gmail.com>
2017-04-10 06:10:58 +0700

    add chunk load data each 3

--- resources/views/shop/index.blade.php
+++ resources/views/shop/index.blade.php
@@ -5,21 +5,23 @@
 @endsection
 
 @section('content')
-    <div class="row">
-        @foreach($products as $product)
-        <div class="col-sm-6 col-md-4">
-            <div class="thumbnail">
-                <img src="{{ $product->imagePath }}" alt="..." class="img-responsive">
-                <div class="caption">
-                    <h3>{{ $product->title }}</h3>
-                    <p class="description">{{ $product->description }}</p>
-                    <div class="clearfix">
-                        <div class="pull-left price">{{ $product->price }}</div>
-                        <a href="#" class="btn btn-success pull-right" role="button">Add to Cart</a>
+    @foreach($products->chunk(3) as $productChunk)
+        <div class="row">
+            @foreach($productChunk as $product)
+                <div class="col-sm-6 col-md-4">
+                    <div class="thumbnail">
+                        <img src="{{ $product->imagePath }}" alt="..." class="img-responsive">
+                        <div class="caption">
+                            <h3>{{ $product->title }}</h3>
+                            <p class="description">{{ $product->description }}</p>
+                            <div class="clearfix">
+                                <div class="pull-left price">${{ $product->price }}</div>
+                                <a href="#" class="btn btn-success pull-right" role="button">Add to Cart</a>
+                            </div>
+                        </div>
                     </div>
                 </div>
-            </div>
+            @endforeach
         </div>
-        @endforeach
-    </div>
+    @endforeach
 @endsection
